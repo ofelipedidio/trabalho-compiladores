639e4d4961da1e38980d7d45b98a8a12dc076bd9
Felipe Didio <felipe.sdidio@gmail.com>
2023-11-25 15:15:40 -0300

    updated main.c

--- main.c
+++ main.c
@@ -8,14 +8,24 @@
  */
 
 #include <stdio.h>
+
 extern int yyparse(void);
 extern int yylex_destroy(void);
+
 void *arvore = NULL;
 void exporta (void *arvore);
+
+struct tree_t {
+
+};
+
 int main (int argc, char **argv)
 {
     int ret = yyparse(); 
     exporta (arvore);
     yylex_destroy();
     return ret;
+}
+
+void exporta (void *arvore) {
 }
