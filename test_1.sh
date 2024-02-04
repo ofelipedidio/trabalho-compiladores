0eab8aa57a7e98b14e2fcbb70f8ab09bc2cba0c6
Felipe Didio <felipe.sdidio@gmail.com>
2024-02-04 20:18:12 -0300

    chore: Removed temp files

--- test_1.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for file in test1/*; do echo -n "$file "; ./etapa5 < $file > /dev/null 2> /dev/null; echo "$?"; done
--- test_2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for file in test1/*; do echo -e "[\033[32m$file\033[0m]"; ./etapa5 < $file; done
