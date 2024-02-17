596178d806fd82d044c08a57dc1d0f2f192f8c73
ofelipedidio <36784465+ofelipedidio@users.noreply.github.com>
2024-02-17 11:55:58 -0300

    Update new_list.h not to use reallocarray

--- new_list.h
+++ new_list.h
@@ -12,13 +12,13 @@
 
 #define nlist_init(list) \
     (list).capacity = 1; \
-    (list).items = (typeof((list).items)) calloc((list).capacity, sizeof(typeof(list.items[0]))); \
+    (list).items = (typeof((list).items)) calloc((list).capacity * sizeof(typeof(list.items[0]))); \
     (list).length = 0;
 
 #define nlist_push(list, item) \
     while ((list).length + 1 >= (list).capacity) { \
         size_t new_capacity = (list).capacity * 2; \
-        typeof((list).items) new_array = (typeof((list).items)) reallocarray((list).items, new_capacity, sizeof(typeof((list).items[0]))); \
+        typeof((list).items) new_array = (typeof((list).items)) realloc((list).items, new_capacity * sizeof(typeof((list).items[0]))); \
         (list).items = new_array; \
         (list).capacity = new_capacity; \
     } \
